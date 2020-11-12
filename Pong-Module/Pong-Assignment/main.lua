6ec9c127b897da174ab1eb69a05bfe1af74bdc8a
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2020-11-12 20:59:58 +0800

    enter

--- Pong-Module/Pong-Assignment/main.lua
+++ Pong-Module/Pong-Assignment/main.lua
@@ -1,14 +1,38 @@
+
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+
 function love.load()
-	love.graphics.setDefaultFilter('nearest', 'nearest')
-	smallfont = love.graphics.newFont('font.ttf', 8)
-	scorefont = love.graphics.newFont('font.ttf', 32)
-	love.graphics.setFont(smallfont)
-	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {fullscreen = false, resizable = false, vsync = true})
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+
+function love.keypressed(key)
+   
+    if key == 'escape' then
+
+        love.event.quit()
+    end
 end
 
 function love.draw()
-	love.graphics.printf('Pong!', 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
+
+    push:apply('start')
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    push:apply('end')
 end
