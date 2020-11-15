38904b4725e8a16e0c842f6d4a9f9dbfea866dee
SOPHIE ANGELA VILLALBA <72681828+sophie-angela-villalba@users.noreply.github.com>
2020-11-15 20:31:38 +0800

    commit

--- Pong-Module/Pong assignment/Ball.lua
+++ Pong-Module/Pong assignment/Ball.lua
@@ -21,3 +21,9 @@ end
 
 function Ball:update(dt)
     self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
