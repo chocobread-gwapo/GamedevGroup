67de9241e60fd4d295a300e1fa365f502b36f905
SOPHIE ANGELA VILLALBA <72681828+sophie-angela-villalba@users.noreply.github.com>
2020-11-15 20:48:25 +0800

    enter

--- Pong-Module/Pong assignment/Paddle.lua
+++ Pong-Module/Pong assignment/Paddle.lua
@@ -7,3 +7,15 @@ function Paddle:init(x, y, width, height)
     self.height = height
     self.dy = 0
 end
+
+function Paddle.update(dt)
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    else
+        self.y = math.min (VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
