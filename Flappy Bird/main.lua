3f12652c18ab3ae85e166784141233d5c5dd4d42
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2020-12-11 05:03:13 +0800

    bird1 update

--- Flappy Bird/main.lua
+++ Flappy Bird/main.lua
@@ -2,12 +2,16 @@ push = require 'push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local background_scroll = 0
 local ground = love.graphics.newImage('ground.png')
+local ground_scroll = 0
+local background_scroll_spd = 30
+local ground_scroll_spd = 60
+local background_loop_point = 413
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -30,11 +34,16 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    background_scroll = (background_scroll + background_scroll_spd * dt) % background_loop_point
+    ground_scroll = (ground_scroll + ground_scroll_spd * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
     
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(background, -background_scroll, 0)
+    love.graphics.draw(ground, -ground_scroll, VIRTUAL_HEIGHT - 16)
     
     push:finish()
 end
