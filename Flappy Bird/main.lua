d0cf251f6069837485d9a9dd6a6a42be899e2d15
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2020-12-11 17:07:27 +0800

    bird10 update

--- Flappy Bird/main.lua
+++ Flappy Bird/main.lua
@@ -8,6 +8,7 @@ require 'states/BaseState'
 require 'states/PlayState'
 require 'states/TitleScreenState'
 require 'states/ScoreState'
+require 'states/CountdownState'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -47,6 +48,7 @@ function love.load()
 
     gStateMachine = StateMachine {
         ['title'] = function() return TitleScreenState() end,
+        ['countdown'] = function() return CountdownState() end,
         ['play'] = function() return PlayState() end,
         ['score'] = function() return ScoreState() end
     }
--- Flappy Bird/states/playstate.lua
+++ Flappy Bird/states/playstate.lua
@@ -10,6 +10,7 @@ function PlayState:init()
     self.bird = Bird()
     self.pipePairs = {}
     self.timer = 0
+    self.score = 0
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
 end
 
