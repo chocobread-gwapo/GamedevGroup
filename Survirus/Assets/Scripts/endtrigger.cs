9b766a4870408e0cd65dae71b599108352a54a2c
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2021-01-30 00:08:38 +0800

    nabalik

---
+++ Survirus/Assets/Scripts/endtrigger.cs
@@ -0,0 +1,15 @@
+﻿//dont forget to include this 
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+
+public class endtrigger : MonoBehaviour
+{
+    private void OnTriggerEnter()
+    {
+        //to start our upcoming scenes..
+	//this is to control our scene order by we add in build settings
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+}
