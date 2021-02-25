d2a1fa2a9acee9544ac5dc264eadf728fab36bd0
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2021-02-25 19:56:56 +0800

    ewan

--- Survirus/Assets/iskrip/endtrigger.cs
+++ Survirus/Assets/iskrip/endtrigger.cs
@@ -7,9 +7,6 @@ public class endtrigger : MonoBehaviour
 {
     private void OnTriggerEnter()
     {
-        //to start our upcoming scenes..
-	//this is to control our scene order by we add in build settings
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
