f267a5648daf937e20241b143fb5ba9692e34993
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2021-02-26 01:39:35 +0800

    Intro Video

---
+++ Survirus/Assets/iskrip/VideoTimerOne.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoTimerOne : MonoBehaviour
+{
+    public int secondsLeft = 30;
+    public bool takingAway = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (takingAway == false && secondsLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+
+        if (secondsLeft == 0)
+        {
+            SceneManager.LoadScene("StartUpLoading");
+        }
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+        if (secondsLeft >= 60)
+        {
+            takingAway = false;
+
+        }
+        else if (secondsLeft < 10)
+        {
+            int seconds = secondsLeft;
+        }
+        else
+        {
+            int seconds = secondsLeft;
+        }
+        takingAway = false;
+    }
+}
