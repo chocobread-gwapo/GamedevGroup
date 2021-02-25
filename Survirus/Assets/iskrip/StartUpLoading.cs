3913044e7d07af32e818d8f3dac73e303d2a87b2
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2021-02-25 22:52:05 +0800

    loading

---
+++ Survirus/Assets/iskrip/StartUpLoading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartUpLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation nextScene = SceneManager.LoadSceneAsync("StartMenu");
+
+        while (nextScene.progress < 1)
+        {
+            progressBar.fillAmount = nextScene.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
